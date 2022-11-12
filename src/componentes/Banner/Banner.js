17a039d41c30a6887a0ea35cddfd4acde9e47e76
Sealtiel Nabuco <sealtielnascimento@gmail.com>
2022-11-12 00:23:29 -0300

    primeiro commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+export const Banner = () => {
+//JXS - Parece HTML, mas não é.
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página so Organo"></img>
+        </header>
+    )
+}
